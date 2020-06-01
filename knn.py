9e0ef8a588c8f94c0268ce5627527383b1d9c8cf
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-06-01 12:09:57 +0200

    Add derivative function and docstrings

--- knn.py
+++ knn.py
@@ -9,24 +9,20 @@ from sklearn.model_selection import train_test_split
 import audiosegment
 
 
-PRED_DEBUG = False   # print result and correctness of every prediction
-# LOUDNESS CONFIG
+PRED_DEBUG = False  # print result and correctness of every prediction
+# POLYFIT CONFIG
 SIZE = 30           # audio snippet size (in ms) for determining loudness/time
 DEGREE = 10         # polynom degree for fitting
 
 # KNN CONFIG
-k = 3              # k neighbors
+k = 3               # k neighbors
 q = 1               # monkowski distance
 w = "distance"      # weighting
-TEST_SIZE = 0.25     # proportion of the dataset to include in the test split
-RANDOM_STATE = 1    # shuffle before split, int for reproducible output
-
-emotion = ["neutral", "calm", "happy", "sad",
-           "angry", "fearful", "disgust", "suprised"]
+TEST_SIZE = 0.25    # proportion of the dataset to include in the test split
+RANDOM_STATE = 0    # shuffle before split, int for reproducible output
 
 
 def main():
-
     # read audio data from file
     data, labels = readData()
 
@@ -39,36 +35,25 @@ def main():
     model.fit(x_train, y_train)
 
     # predicting
-    accuracy = 0
     prediction = model.predict(x_test)
     prediction_prob = model.predict_proba(x_test)
 
-    # print results
-    for i in range(0, len(prediction)):
-        if y_test[i] == prediction[i]:
-            result = "TRUE"
-            accuracy += 1
-        else:
-            result = "FALSE"
-        if PRED_DEBUG:
-            print("[{:5s}]\nprediction={} key={}\nprob={}\n\t   {}\n---\n".format(result,
-                                                                                  emotion[prediction[i] -
-                                                                                          1].upper(),
-                                                                                  emotion[y_test[i] -
-                                                                                          1].upper(),
-                                                                                  np.array_repr(
-                                                                                      prediction_prob[i]).replace('\n', ''),
-                                                                                  emotion))
-    print("\t### RESULT ###\n")
-    print("\t{:3.2f}% ACCURACY [{}/{}]".format((accuracy /
-                                                len(x_test))*100,
-                                               accuracy,
-                                               len(x_test)))
-    print("\tn_train={} n_test={}".format(len(x_train), len(x_test)))
-    print_config()
+    # print and evaluate results
+    evalPrediction(prediction, prediction_prob, x_test, y_test, len(data))
+    printConfig()
 
 
 def readData():
+    """
+    ! FILE STRUCTURE HARDCODED !
+    ----------------------------
+    Function reads all available audio files and returns an array with all relevant data points:
+        - Frequency (Over time, Maximum, Average)
+        - Loudness (Over time, Maximum, Average)
+
+    Returns:
+        (array, array) -- Returns a tuple consisting of data and related labels
+    """
     counter = 0     # loop index
     data = []       # list of data used for training/testing
     labels = []     # list of labels (correct emotions) related to traindata
@@ -80,19 +65,29 @@ def readData():
                     "audio/{}/{}".format(p, f), format="wav")
                 s2 = audiosegment.from_file(
                     "audio/{}/{}".format(p, f))
-                freqChange, freqMax, freqAvg = getFreqChange(s2)
-                fdata = parse_fname(f)
-                loudPoly, dBFS, maxDBFS = get_loudpoly(s1)
-                data.append(loudPoly + freqChange + dBFS +
-                            maxDBFS + freqMax + freqAvg)
-                # Loudness polynom parameter, Freq polynom parameter, avg loudness in db, max loudness, max freq, avg freq
+                freqChange, freqAvg, freqMax = getFrequency(s2)
+                fdata = parseFName(f)
+                loudPoly, dBFS, maxDBFS = getLoudness(s1)
+                data.append(np.append(np.concatenate((loudPoly, getDerivative(
+                    loudPoly), freqChange, getDerivative(freqChange))), [dBFS, maxDBFS, freqAvg, freqMax]))
                 labels.append(fdata.get("emotion_n"))
                 counter += 1
         print("{}: {} files".format(p, counter))
     return data, labels
 
 
-def get_loudpoly(s):
+def getLoudness(s):
+    """Returns Loudness attributes:
+        - coefficients of a curve fitted to loudness over time
+        - average decibel relative to the maximum possible loudness
+        - maximum decibel
+
+    Arguments:
+        s {AudioSegment} -- Audio as AudioSegment object
+
+    Returns:
+        (tuple, float, float) -- ((coefficients), dBFS, maxDBFS)
+    """
     xdata = []
     ydata = []
     chunks = make_chunks(s, SIZE)
@@ -104,7 +99,18 @@ def get_loudpoly(s):
     return p, s.dBFS, s.max_dBFS
 
 
-def getFreqChange(s):
+def getFrequency(s):
+    """Return Frequency attributes:
+        - coefficients of a curve fitted to frequency over time
+        - maximum frequency
+        - average frequency
+
+    Arguments:
+        s {AudioSegment} -- Audio as AudioSegment object
+
+    Returns:
+        (tuple, float, float) -- ((coefficients), maxFreq, avgFreq)
+    """
     xdata = []
     ydata = []
     chunks = make_chunks(s, SIZE)
@@ -120,11 +126,13 @@ def getFreqChange(s):
     p = np.polyfit(np.array(xdata), np.array(ydata), DEGREE)
     max_value = np.amax(np.array(ydata))
     avg_value = np.mean(np.array(ydata))
-    return p, max_value, avg_value
+    return p, avg_value, max_value
 
 
-def parse_fname(fname):
+def parseFName(fname):
     '''
+    ! PLACEHOLDER FUNCTION !
+    ------------------------
     # 03-01-01-01-01-01-01.wav
     Modality (01 = full-AV, 02 = video-only, 03 = audio-only).
     Vocal channel (01 = speech, 02 = song).
@@ -158,7 +166,9 @@ def parse_fname(fname):
     return fdict
 
 
-def print_config():
+def printConfig():
+    """Outputs all relevant values of the current configuration
+    """
     print("\n# LOUDNESS")
     print("SIZE = {}".format(SIZE))
     print("DEGREE = {}".format(DEGREE))
@@ -169,14 +179,60 @@ def print_config():
     print("TEST_SIZE = {}".format(TEST_SIZE))
     print("RANDOM_STATE = {}".format(RANDOM_STATE))
 
-def derivative(poly):
-    #This function calculates the derivative of a n-th degree polynomial 
-    #'poly' is an array in the form of e.g. [a, b, c] where ax^2+bx+c
-    #An array in similar form like 'poly' is returned.
-    if len(poly)==1:
+
+def getDerivative(poly):
+    """This function calculates the derivative of a n-th degree polynomial
+
+    Arguments:
+        poly {array} -- Polynomial coefficients, in the form of e.g. [a, b, c] where ax^2+bx+c.
+
+    Returns:
+        array -- Polynomial coefficients of the derivative, highest power first.
+    """
+    # This function calculates the derivative of a n-th degree polynomial
+    # 'poly' is an array in the form of e.g. [a, b, c] where ax^2+bx+c
+    # An array in similar form like 'poly' is returned.
+    if len(poly) == 1:
         return [0]
     deriv_poly = [poly[i] * (len(poly)-i-1) for i in range(0, len(poly)-1)]
     return deriv_poly
+
+
+def evalPrediction(prediction, prediction_prob, x_test, y_test, data_n):
+    """Print test results, including prediction results
+
+    Arguments:
+        prediction {array} -- Class labels for each data sample
+        prediction_prob {array} -- The class probabilities of the input samples.
+        x_test {array} -- Data samples used for testing
+        y_test {array} -- Labels related to testing data samples
+        data_n {int} -- Length of overall dataset (training + testing)
+    """
+    emotion = ["neutral", "calm", "happy", "sad",
+               "angry", "fearful", "disgust", "suprised"]
+    accuracy = 0
+    x_test_n = len(x_test)
+    for i in range(0, len(prediction)):
+        if y_test[i] == prediction[i]:
+            result = "TRUE"
+            accuracy += 1
+        else:
+            result = "FALSE"
+        if PRED_DEBUG:
+            print("[{:5s}]\nprediction={} key={}\nprob={}\n\t   {}\n---\n".format(result,
+                                                                                  emotion[prediction[i] -
+                                                                                          1].upper(),
+                                                                                  emotion[y_test[i] -
+                                                                                          1].upper(),
+                                                                                  np.array_repr(
+                                                                                      prediction_prob[i]).replace('\n', ''),
+                                                                                  emotion))
+    print("\t### RESULT ###\n")
+    print("\t{:3.2f}% ACCURACY [{}/{}]".format((accuracy /
+                                                x_test_n)*100,
+                                               accuracy,
+                                               x_test_n))
+    print("\tn_train={} n_test={}".format(data_n-x_test_n, x_test_n))
 
 
 if __name__ == "__main__":

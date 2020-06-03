dc6333d2cad76847db53aa584e88ee2885c1fd26
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-06-03 19:39:12 +0200

    Add configparser and multiple databases

--- knn.py
+++ knn.py
@@ -1,37 +1,35 @@
+import configparser
 import os
+import sys
 
-import matplotlib.pyplot as plt
+import audiosegment
 import numpy as np
 from pydub import AudioSegment
 from pydub.utils import make_chunks
-from sklearn.neighbors import KNeighborsClassifier as wknn
 from sklearn.model_selection import train_test_split
-import audiosegment
-
-
-PRED_DEBUG = False  # print result and correctness of every prediction
-# POLYFIT CONFIG
-SIZE = 30           # audio snippet size (in ms) for determining loudness/time
-DEGREE = 10         # polynom degree for fitting
-
-# KNN CONFIG
-k = 3               # k neighbors
-q = 1               # monkowski distance
-w = "distance"      # weighting
-TEST_SIZE = 0.25    # proportion of the dataset to include in the test split
-RANDOM_STATE = 0    # shuffle before split, int for reproducible output
+from sklearn.neighbors import KNeighborsClassifier as wknn
 
 
 def main():
+    cfg = loadConfig()
+    # TODO blackbox class etc
+    blackBox(config=cfg)
+
+
+def blackBox(config):
     # read audio data from file
-    data, labels = readData()
+    data, labels = [], []
+    for db in config['DATABASES']:
+        loop_data, loop_labels = readData(db, config)
+        data, labels = data + loop_data, labels + loop_labels
 
     # split data into test and train
     x_train, x_test, y_train, y_test = train_test_split(
-        data, labels, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+        data, labels, test_size=float(config['KNN']['test_size']), random_state=int(config['KNN']['random_state']))
 
     # train model
-    model = wknn(n_neighbors=k, weights=w, p=q)
+    model = wknn(n_neighbors=int(config["KNN"]["k"]),
+                 weights=config["KNN"]["w"], p=int(config["KNN"]["q"]))
     model.fit(x_train, y_train)
 
     # predicting
@@ -39,11 +37,27 @@ def main():
     prediction_prob = model.predict_proba(x_test)
 
     # print and evaluate results
-    evalPrediction(prediction, prediction_prob, x_test, y_test, len(data))
-    printConfig()
+    evalPrediction(prediction, prediction_prob, x_test, y_test, len(
+        data), int(config['DEBUG']['prediction_details']))
+    printConfig(config)
 
 
-def readData():
+def loadConfig():
+    """Loads the config file and checks if the file is valid
+
+    Returns:
+        [configObject] -- [Config file as object]
+    """    
+    cfg = configparser.ConfigParser(allow_no_value=True)
+    cfile = cfg.read("config.ini")
+    # TODO check for valid config
+    if not cfile:
+        print("ERR: Failed loading config")
+        sys.exit()
+    return cfg
+
+
+def readData(database, config):
     """
     ! FILE STRUCTURE HARDCODED !
     ----------------------------
@@ -55,28 +69,37 @@ def readData():
         (array, array) -- Returns a tuple consisting of data and related labels
     """
     counter = 0     # loop index
+    error = 0       # corrupted files
     data = []       # list of data used for training/testing
     labels = []     # list of labels (correct emotions) related to traindata
     #                   (eg traindata[0] is labels[0] emotion)
-    for p in os.listdir("audio"):
-        if not os.path.isfile(p):
-            for f in os.listdir("audio/"+p):
-                s1 = AudioSegment.from_file(
-                    "audio/{}/{}".format(p, f), format="wav")
-                s2 = audiosegment.from_file(
-                    "audio/{}/{}".format(p, f))
-                freqChange, freqAvg, freqMax = getFrequency(s2)
-                fdata = parseFName(f)
-                loudPoly, dBFS, maxDBFS = getLoudness(s1)
-                data.append(np.append(np.concatenate((loudPoly, getDerivative(
-                    loudPoly), freqChange, getDerivative(freqChange))), [dBFS, maxDBFS, freqAvg, freqMax]))
-                labels.append(fdata.get("emotion_n"))
-                counter += 1
-        print("{}: {} files".format(p, counter))
+    for f in os.listdir("audio/"+database):
+        try:
+            s1 = AudioSegment.from_file(
+                "audio/{}/{}".format(database, f), format="wav")
+            s2 = audiosegment.from_file("audio/{}/{}".format(database, f))
+        except FileNotFoundError:
+            error += 1
+            print("[{} ({})] {}/{}".format(counter,
+                                           error, database, f), end="\r")
+            continue
+        freqChange, freqAvg, freqMax = getFrequency(s2, int(
+            config['POLYFIT']['snippet_size']), int(config['POLYFIT']['poly_degree']))
+        fdata = parseFName(f)
+        loudPoly, dBFS, maxDBFS = getLoudness(s1, int(
+            config['POLYFIT']['snippet_size']), int(config['POLYFIT']['poly_degree']))
+        data.append(np.append(np.concatenate((getDerivative(
+                    loudPoly),  getDerivative(freqChange))), [dBFS, maxDBFS, freqAvg, freqMax]))
+        labels.append(fdata.get("emotion_n"))
+        counter += 1
+        print("[{} ({})] Loading {}/{}...\t\t\t".format(counter,
+                                                        error, database, f), end="\r")
+    print("[{} ({})] Finished loading {}\t\t\t\n".format(counter,
+                                                         error, database), end="\r")
     return data, labels
 
 
-def getLoudness(s):
+def getLoudness(s, n, degree):
     """Returns Loudness attributes:
         - coefficients of a curve fitted to loudness over time
         - average decibel relative to the maximum possible loudness
@@ -90,16 +113,16 @@ def getLoudness(s):
     """
     xdata = []
     ydata = []
-    chunks = make_chunks(s, SIZE)
+    chunks = make_chunks(s, n)
     for i in range(0, len(chunks)):
         if chunks[i].dBFS != -np.inf:
-            xdata.append((i+1)*SIZE)
+            xdata.append((i+1)*n)
             ydata.append(chunks[i].dBFS)
-    p = np.polyfit(np.array(xdata), np.array(ydata), DEGREE)
+    p = np.polyfit(np.array(xdata), np.array(ydata), degree)
     return p, s.dBFS, s.max_dBFS
 
 
-def getFrequency(s):
+def getFrequency(s, n, degree):
     """Return Frequency attributes:
         - coefficients of a curve fitted to frequency over time
         - maximum frequency
@@ -113,7 +136,7 @@ def getFrequency(s):
     """
     xdata = []
     ydata = []
-    chunks = make_chunks(s, SIZE)
+    chunks = make_chunks(s, n)
     for i in range(0, len(chunks)):
         void, hist_vals = chunks[i].fft()
         del void
@@ -121,63 +144,50 @@ def getFrequency(s):
 
         avg = np.mean(hist_vals_real_normed)
 
-        xdata.append((i+1)*SIZE)
+        xdata.append((i+1)*n)
         ydata.append(avg)
-    p = np.polyfit(np.array(xdata), np.array(ydata), DEGREE)
+    p = np.polyfit(np.array(xdata), np.array(ydata), degree)
     max_value = np.amax(np.array(ydata))
     avg_value = np.mean(np.array(ydata))
     return p, avg_value, max_value
 
 
-def parseFName(fname):
-    '''
-    ! PLACEHOLDER FUNCTION !
-    ------------------------
-    # 03-01-01-01-01-01-01.wav
-    Modality (01 = full-AV, 02 = video-only, 03 = audio-only).
-    Vocal channel (01 = speech, 02 = song).
-    Emotion (01 = neutral, 02 = calm, 03 = happy, 04 = sad, 05 = angry, 06 = fearful, 07 = disgust, 08 = surprised).
-    Emotional intensity (01 = normal, 02 = strong). NOTE: There is no strong intensity for the 'neutral' emotion.
-    Statement (01 = "Kids are talking by the door", 02 = "Dogs are sitting by the door").
-    Repetition (01 = 1st repetition, 02 = 2nd repetition).
-    Actor (01 to 24. Odd numbered actors are male, even numbered actors are female).
-    '''
-    modality = ["full-AV", "video", "audio"]
-    vocal = ["speech", "song"]
-    emotion = ["neutral", "calm", "happy", "sad",
-               "angry", "fearful", "disgust", "suprised"]
-    intensity = ["normal", "strong"]
-    statement = ["kids", "dogs"]
-    repetition = ["1st rep", "2nd rep"]
-    actor = ["female", "male"]
+def parseFName(name):
+    """Parse the file name extracting information about the audio file import for labeling. Only works if the naming scheme is correct!
 
-    fname = fname.strip(".wav")
-    farray = fname.split("-")
+    Arguments:
+        name {file} -- File
+
+    Returns:
+        dict -- Returns dict with emotion, actor and id
+    """    
+    emotion = ["happy", "suprise", "angry", "sad",
+               "fear", "disgust", "neutral"]
+    actor = ["male", "female"]
+    name = name.strip(".wav")
+    # doing this twice beacuse some f'd the toronto dataset
+    name = name.strip(".wav")
+    fdata = name.split("-")
     fdict = {
-        "modality": modality[int(farray[0])-1],
-        "vocal": vocal[int(farray[1])-1],
-        "emotion": emotion[int(farray[2])-1],
-        "emotion_n": int(farray[2]),
-        "intensity": intensity[int(farray[3])-1],
-        "statement": statement[int(farray[4])-1],
-        "repetition": repetition[int(farray[5])-1],
-        "actor": 1 if (int(farray[6]) % 2 == 0) else 0  # 0 = blue , 1 = red
+        "emotion": emotion[int(fdata[0])-1],
+        "emotion_n": int(fdata[0]),
+        "actor": actor[int(fdata[1])-1],
+        "actor_n": int(fdata[1])-1,
+        "id": fdata[2]
     }
     return fdict
 
 
-def printConfig():
+def printConfig(config):
     """Outputs all relevant values of the current configuration
     """
-    print("\n# LOUDNESS")
-    print("SIZE = {}".format(SIZE))
-    print("DEGREE = {}".format(DEGREE))
-    print("# KNN CONFIG")
-    print("k = {}".format(k))
-    print("q = {}".format(q))
-    print("w = {}".format(w))
-    print("TEST_SIZE = {}".format(TEST_SIZE))
-    print("RANDOM_STATE = {}".format(RANDOM_STATE))
+    for section in config.sections():
+        print("[{}]".format(section))
+        for key in config[section]:
+            if config[section][key] == None:
+                print("{}".format(key))
+            else: 
+                 print("{}={}".format(key, config[section][key]))
 
 
 def getDerivative(poly):
@@ -189,16 +199,13 @@ def getDerivative(poly):
     Returns:
         array -- Polynomial coefficients of the derivative, highest power first.
     """
-    # This function calculates the derivative of a n-th degree polynomial
-    # 'poly' is an array in the form of e.g. [a, b, c] where ax^2+bx+c
-    # An array in similar form like 'poly' is returned.
     if len(poly) == 1:
         return [0]
     deriv_poly = [poly[i] * (len(poly)-i-1) for i in range(0, len(poly)-1)]
     return deriv_poly
 
 
-def evalPrediction(prediction, prediction_prob, x_test, y_test, data_n):
+def evalPrediction(prediction, prediction_prob, x_test, y_test, data_n, debug=False):
     """Print test results, including prediction results
 
     Arguments:
@@ -208,8 +215,8 @@ def evalPrediction(prediction, prediction_prob, x_test, y_test, data_n):
         y_test {array} -- Labels related to testing data samples
         data_n {int} -- Length of overall dataset (training + testing)
     """
-    emotion = ["neutral", "calm", "happy", "sad",
-               "angry", "fearful", "disgust", "suprised"]
+    emotion = ["happy", "suprise", "angry", "sad",
+               "fear", "disgust", "neutral"]
     accuracy = 0
     x_test_n = len(x_test)
     for i in range(0, len(prediction)):
@@ -218,7 +225,7 @@ def evalPrediction(prediction, prediction_prob, x_test, y_test, data_n):
             accuracy += 1
         else:
             result = "FALSE"
-        if PRED_DEBUG:
+        if debug:
             print("[{:5s}]\nprediction={} key={}\nprob={}\n\t   {}\n---\n".format(result,
                                                                                   emotion[prediction[i] -
                                                                                           1].upper(),

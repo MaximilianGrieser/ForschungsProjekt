d3c0f2711b50ce6644e8848d113f20cf113dcec3
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-06-04 14:34:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from blackbox import BlackBox
 
@@ -7,19 +8,40 @@ def main():
     blackbox = BlackBox(config="repack/config.ini")
     blackbox.loadData(database="Toronto-DB")
     blackbox.loadData(database="RAVDESS-DB")
-    accuracy = []
-    f = open("testruns.log", "a")
-    for i in range(1, 1001):
+
+    ###
+    # Config Parameter test
+    ###
+    xdata = []
+    ydata = []
+    for n in range(1, 51):
+        blackbox.updateConfig("KNN", "k", n, wipe=False)
         blackbox.train()
         blackbox.predict()
-        f.write("{:5s} {:3.5f} {}\n".format(
-            str(i), blackbox.accuracy, blackbox.databases_loaded))
-        print(i, end="\r")
-        accuracy.append(blackbox.accuracy)
-    print("\n")
-    print(np.mean(accuracy))
-    f.write(f"### AVG {np.mean(accuracy)} ### \n")
-    f.close()
+        xdata.append(n)
+        ydata.append(blackbox.accuracy)
+        print(f"k={n}\t\t", end="\r")
+    plt.plot(xdata, ydata, "b")
+    plt.ylabel("\naccuracy")
+    plt.xlabel("k neighbors")
+    plt.show()
+
+    ###
+    # Accuracy average test
+    ###
+    #accuracy = []
+    #f = open("testruns.log", "a")
+    # for i in range(1, 1001):
+    #    blackbox.train()
+    #    blackbox.predict()
+    #    f.write("{:5s} {:3.5f} {}\n".format(
+    #        str(i), blackbox.accuracy, blackbox.databases_loaded))
+    #    print(i, end="\r")
+    #    accuracy.append(blackbox.accuracy)
+    # print("\n")
+    # print(np.mean(accuracy))
+    # f.write(f"### AVG {np.mean(accuracy)} ### \n")
+    # f.close()
 
 
 if __name__ == "__main__":

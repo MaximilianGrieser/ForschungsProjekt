0e5ede644b149572d8bd9e53e75a7650ae27c542
Jonas Rheiner <39375864+jrheiner@users.noreply.github.com>
2020-06-04 10:35:24 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from blackbox import BlackBox
+import numpy as np
+
+
+def main():
+    blackbox = BlackBox(config="repack/config.ini")
+    blackbox.loadData(database="Toronto-DB")
+    blackbox.loadData(database="RAVDESS-DB")
+    accuracy = []
+    f = open("testruns.log", "a")
+    for i in range(1, 1001):
+        blackbox.train()
+        blackbox.predict()
+        f.write("{:5s} {:3.5f} {}\n".format(
+            str(i), blackbox.accuracy, blackbox.databases_loaded))
+        print(i, end="\r")
+        accuracy.append(blackbox.accuracy)
+    print("\n")
+    print(np.mean(accuracy))
+    f.write(f"### AVG {np.mean(accuracy)} ### \n")
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
